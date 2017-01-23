ca975a21817557da8f960a080a52e6b0bae0dc8e
abhineetmittal <abhineetmittal@gmail.com>
2017-01-23 08:26:34 +0530

    Update unicode-word-count.php

--- unicode-word-count.php
+++ unicode-word-count.php
@@ -8,7 +8,7 @@ function count_unicode_words( $unicode_string ){
   $unicode_string = preg_replace('/[[:space:]]/', ' ', $unicode_string);
   // The words are now separated by single spaces and can be splitted to an array
   // On the safer side, I have included \n\r\t here is well, only space will also suffice
-  $words_array = preg_split( "/[\n\r\t ]+/", $post_content, 0, PREG_SPLIT_NO_EMPTY );
+  $words_array = preg_split( "/[\n\r\t ]+/", $unicode_string, 0, PREG_SPLIT_NO_EMPTY );
   // Now we can get the word count by counting array elments
   return count($words_array);
 }
